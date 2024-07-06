2e756d8d42b3799f6624cd26f12ea8809526975f
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-07-06 19:25:51 +0100

    - Connect student manager with student register

--- DesktopAPP/ui/windows/student_manager/student_manager.py
+++ DesktopAPP/ui/windows/student_manager/student_manager.py
@@ -1,14 +1,16 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtCore import Slot
 from .student_manager_ui import Ui_StudentManager
 
 class StudentManager(Ui_StudentManager, QMainWindow):
 	def __init__(self):
 		super(StudentManager, self).__init__()
 		self.setupUi(self)
-		self.register_button.clicked.connect()
-		self.search_button.clicked.connect()
-		self.delete_selected_button.clicked.connect()
-		self.selected_all_checkBox.checkStateChanged.connect()
+		self.register_students_window = None
+		self.register_button.clicked.connect(self.show_regiter_student_window)
+		self.search_button.clicked.connect(self.search_student)
+		self.delete_selected_button.clicked.connect(self.search_selected_student)
+		# self.selected_all_checkBox.checkStateChanged.connect()
 	
 		self.show()
 	
@@ -24,5 +26,15 @@ class StudentManager(Ui_StudentManager, QMainWindow):
 	def search_student(self):
 		pass
 
-	def delete_student(self):
+	def search_selected_student(self):
 		pass
+	
+	def show_regiter_student_window (self):
+		from ..student_register_info.student_register_info import StudentRegisterInfo
+		if self.register_students_window is None:
+			self.register_students_window = StudentRegisterInfo()
+			self.register_students_window.window_closed.connect(self.register_students_window_closed)
+
+	@Slot()
+	def register_students_window_closed(self):
+		self.register_students_window = None
