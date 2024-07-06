3ecbd3cffc4f502a8847f7a38680ec58ffc0ef45
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-07-06 15:46:01 +0100

    - Switch from scrole area widget to the list widget - Rename objects name

--- DesktopAPP/ui/windows/student_manager/student_manager.py
+++ DesktopAPP/ui/windows/student_manager/student_manager.py
@@ -5,4 +5,24 @@ class StudentManager(Ui_StudentManager, QMainWindow):
 	def __init__(self):
 		super(StudentManager, self).__init__()
 		self.setupUi(self)
+		self.register_button.clicked.connect()
+		self.search_button.clicked.connect()
+		self.delete_selected_button.clicked.connect()
+		self.selected_all_checkBox.checkStateChanged.connect()
+	
 		self.show()
+	
+	def load_student_from_db (self):
+		#TODO: Load studetn from db 
+		# Create card foe each
+		# Load there info 
+		pass
+
+	def register_student(self):
+		pass
+
+	def search_student(self):
+		pass
+
+	def delete_student(self):
+		pass
--- DesktopAPP/ui/windows/student_manager/student_manager_ui.py
+++ DesktopAPP/ui/windows/student_manager/student_manager_ui.py
@@ -17,8 +17,9 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QCheckBox, QComboBox, QFrame,
     QGridLayout, QHBoxLayout, QLabel, QLineEdit,
-    QMainWindow, QPushButton, QScrollArea, QSizePolicy,
-    QSpacerItem, QStatusBar, QVBoxLayout, QWidget)
+    QListView, QListWidget, QListWidgetItem, QMainWindow,
+    QPushButton, QSizePolicy, QSpacerItem, QStatusBar,
+    QWidget)
 
 class Ui_StudentManager(object):
     def setupUi(self, StudentManager):
@@ -27,8 +28,8 @@ class Ui_StudentManager(object):
         StudentManager.resize(767, 558)
         self.centralwidget = QWidget(StudentManager)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.verticalLayout = QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.gridLayout_2 = QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.horizontalLayout_2 = QHBoxLayout()
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.horizontalLayout = QHBoxLayout()
@@ -38,25 +39,28 @@ class Ui_StudentManager(object):
 
         self.horizontalLayout.addWidget(self.label)
 
-        self.comboBox = QComboBox(self.centralwidget)
-        self.comboBox.setObjectName(u"comboBox")
+        self.degree_combo_box = QComboBox(self.centralwidget)
+        self.degree_combo_box.addItem("")
+        self.degree_combo_box.addItem("")
+        self.degree_combo_box.addItem("")
+        self.degree_combo_box.setObjectName(u"degree_combo_box")
 
-        self.horizontalLayout.addWidget(self.comboBox)
+        self.horizontalLayout.addWidget(self.degree_combo_box)
 
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
 
         self.horizontalLayout.addWidget(self.label_2)
 
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
+        self.name_input = QLineEdit(self.centralwidget)
+        self.name_input.setObjectName(u"name_input")
 
-        self.horizontalLayout.addWidget(self.lineEdit)
+        self.horizontalLayout.addWidget(self.name_input)
 
-        self.pushButton_2 = QPushButton(self.centralwidget)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.search_button = QPushButton(self.centralwidget)
+        self.search_button.setObjectName(u"search_button")
 
-        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.horizontalLayout.addWidget(self.search_button)
 
 
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
@@ -65,32 +69,32 @@ class Ui_StudentManager(object):
 
         self.horizontalLayout_2.addItem(self.horizontalSpacer)
 
-        self.pushButton_3 = QPushButton(self.centralwidget)
-        self.pushButton_3.setObjectName(u"pushButton_3")
+        self.register_button = QPushButton(self.centralwidget)
+        self.register_button.setObjectName(u"register_button")
 
-        self.horizontalLayout_2.addWidget(self.pushButton_3)
+        self.horizontalLayout_2.addWidget(self.register_button)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        self.gridLayout_2.addLayout(self.horizontalLayout_2, 0, 0, 1, 1)
 
         self.horizontalLayout_3 = QHBoxLayout()
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.checkBox = QCheckBox(self.centralwidget)
-        self.checkBox.setObjectName(u"checkBox")
+        self.selected_all_checkBox = QCheckBox(self.centralwidget)
+        self.selected_all_checkBox.setObjectName(u"selected_all_checkBox")
 
-        self.horizontalLayout_3.addWidget(self.checkBox)
+        self.horizontalLayout_3.addWidget(self.selected_all_checkBox)
 
-        self.pushButton = QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(u"pushButton")
+        self.delete_selected_button = QPushButton(self.centralwidget)
+        self.delete_selected_button.setObjectName(u"delete_selected_button")
 
-        self.horizontalLayout_3.addWidget(self.pushButton)
+        self.horizontalLayout_3.addWidget(self.delete_selected_button)
 
         self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
 
         self.horizontalLayout_3.addItem(self.horizontalSpacer_2)
 
 
-        self.verticalLayout.addLayout(self.horizontalLayout_3)
+        self.gridLayout_2.addLayout(self.horizontalLayout_3, 1, 0, 1, 1)
 
         self.frame = QFrame(self.centralwidget)
         self.frame.setObjectName(u"frame")
@@ -98,18 +102,19 @@ class Ui_StudentManager(object):
         self.frame.setFrameShadow(QFrame.Shadow.Raised)
         self.gridLayout = QGridLayout(self.frame)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.scrollArea = QScrollArea(self.frame)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 739, 421))
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.student_card_info_grid = QListWidget(self.frame)
+        self.student_card_info_grid.setObjectName(u"student_card_info_grid")
+        self.student_card_info_grid.setMovement(QListView.Movement.Snap)
+        self.student_card_info_grid.setFlow(QListView.Flow.LeftToRight)
+        self.student_card_info_grid.setProperty("isWrapping", True)
+        self.student_card_info_grid.setResizeMode(QListView.ResizeMode.Adjust)
+        self.student_card_info_grid.setLayoutMode(QListView.LayoutMode.Batched)
+        self.student_card_info_grid.setViewMode(QListView.ViewMode.ListMode)
 
-        self.gridLayout.addWidget(self.scrollArea, 0, 0, 1, 1)
+        self.gridLayout.addWidget(self.student_card_info_grid, 0, 0, 1, 1)
 
 
-        self.verticalLayout.addWidget(self.frame)
+        self.gridLayout_2.addWidget(self.frame, 2, 0, 1, 1)
 
         StudentManager.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(StudentManager)
@@ -123,11 +128,15 @@ class Ui_StudentManager(object):
 
     def retranslateUi(self, StudentManager):
         StudentManager.setWindowTitle(QCoreApplication.translate("StudentManager", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("StudentManager", u"TextLabel", None))
-        self.label_2.setText(QCoreApplication.translate("StudentManager", u"TextLabel", None))
-        self.pushButton_2.setText(QCoreApplication.translate("StudentManager", u"PushButton", None))
-        self.pushButton_3.setText(QCoreApplication.translate("StudentManager", u"PushButton", None))
-        self.checkBox.setText(QCoreApplication.translate("StudentManager", u"CheckBox", None))
-        self.pushButton.setText(QCoreApplication.translate("StudentManager", u"PushButton", None))
+        self.label.setText(QCoreApplication.translate("StudentManager", u"Degree", None))
+        self.degree_combo_box.setItemText(0, QCoreApplication.translate("StudentManager", u"All", None))
+        self.degree_combo_box.setItemText(1, QCoreApplication.translate("StudentManager", u"Bachelor", None))
+        self.degree_combo_box.setItemText(2, QCoreApplication.translate("StudentManager", u"Master", None))
+
+        self.label_2.setText(QCoreApplication.translate("StudentManager", u"Name", None))
+        self.search_button.setText(QCoreApplication.translate("StudentManager", u"search", None))
+        self.register_button.setText(QCoreApplication.translate("StudentManager", u"Register", None))
+        self.selected_all_checkBox.setText("")
+        self.delete_selected_button.setText(QCoreApplication.translate("StudentManager", u"Delete Selected", None))
     # retranslateUi
 
