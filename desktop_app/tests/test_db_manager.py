ee0e180f68206d311d3b7009d6d00a2a20a1797e
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-07-14 19:55:02 +0100

    - Add new test case

--- desktop_app/tests/test_db_manager.py
+++ desktop_app/tests/test_db_manager.py
@@ -64,19 +64,48 @@ def test_add_students(db_setup):
 
 def test_modify_student_with_no_picture(db_setup):
     # Your test logic here
-    alice_id = 1  # Assuming this is Alice's ID
+    student_id = 1  # Assuming this is Alice's ID
     modified_info = {'name': 'Alice Johnson', 'speciality': 'Astrophysics and Cosmology'}
-    db_setup.modify_student(alice_id, modified_info)
-    alice = db_setup.get_student_by_id(alice_id)
+    db_setup.modify_student(student_id, modified_info)
+    alice = db_setup.get_student_by_id(student_id)
     assert alice[1] == 'Alice Johnson', "Name not updated correctly."
     assert alice[5] == 'Astrophysics and Cosmology', "Speciality not updated correctly."
 
 def test_modify_student_with_picture(db_setup):
     # Your test logic here
-    alice_id = 1  # Assuming this is Alice's ID
+    student_id = 1  # Assuming this is Alice's ID
+    student_info = db_setup.get_student_by_id(student_id)
+    student_name = student_info[1].replace(' ', '_')
+    student_birthday = student_info[2]
+    student_picture_path = f'{db_setup.picture_directory}/{student_name}_{student_birthday}.jpg'
+    prev_time = os.stat(student_picture_path).st_ctime
     modified_info = {'name': 'Johnson', 'speciality': 'Informatics','picture_path': "/home/abdelhake/Pictures/Wallpapers/SAVE_20231117_150231.jpg"}
-    db_setup.modify_student(alice_id, modified_info)
-    alice = db_setup.get_student_by_id(alice_id)
+    db_setup.modify_student(student_id, modified_info)
+    alice = db_setup.get_student_by_id(student_id)
+    student_info = db_setup.get_student_by_id(student_id)
+    student_name = student_info[1].replace(' ', '_')
+    student_birthday = student_info[2]
+    student_picture_path = f'{db_setup.picture_directory}/{student_name}_{student_birthday}.jpg'
+    current_time = os.stat(student_picture_path).st_ctime
     assert alice[1] == 'Johnson', "Name not updated correctly."
     assert alice[5] == 'Informatics', "Speciality not updated correctly."
+    assert prev_time != current_time, "Picture wasn't updated "
+
+def test_delete_student(db_setup):
+    student_id = 1
+    student_info = db_setup.get_student_by_id(student_id)
+    db_setup.delete_student(student_id)
+    student_name = student_info[1].replace(' ', '_')
+    student_birthday = student_info[2]
+    student_picture_path = f'{db_setup.picture_directory}/{student_name}_{student_birthday}.jpg'
+    picture_not_existe = True
+    if os.path.exists(student_picture_path):
+        picture_not_existe = False
+    print(picture_not_existe)
+    assert db_setup.get_student_by_id(student_id) is None , \
+        f"Student didn't deleted Expected None but got {db_setup.get_student_by_id(student_id)}"
+    assert picture_not_existe, "Image wasn't deleted successfully!" 
+
+    
+    
 
