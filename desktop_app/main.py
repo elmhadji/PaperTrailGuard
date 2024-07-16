d538b122a0c0329c57e5b2cc755f31487f391ca3
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-07-16 19:09:20 +0100

    - Add student directly when staring app for testing purpose

--- desktop_app/main.py
+++ desktop_app/main.py
@@ -9,6 +9,21 @@ class Main(Login, QMainWindow):
 		self.username_input.setText('admin')
 		self.password_input.setText('admin')
 		self.login()
+		self.student_manager_window.show_regiter_student_window()
+		
+		# self.student_manager_window.register_students_window.name_input.setText('geregr')
+		# self.student_manager_window.register_students_window.birth_place_input.setText('geregr')
+		# self.student_manager_window.register_students_window.university_input.setText('geregr') 
+		# self.student_manager_window.register_students_window.speciality_input.setText('geregr')
+		# self.student_manager_window.register_students_window.domain_input.setText('geregr')
+		# self.student_manager_window.register_students_window.sector_input.setText('geregr') 
+		# self.student_manager_window.register_students_window.departement_input.setText('geregr') 
+		# self.student_manager_window.register_students_window.degree_combobox.setCurrentIndex(1), 
+		# self.student_manager_window.register_students_window.note_input.setText('geregr') 
+		# self.student_manager_window.register_students_window.picture_path = "desktop_app/tests/test_images/alice.jpg"
+		
+		# self.student_manager_window.register_students_window.register_student()
+		
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
