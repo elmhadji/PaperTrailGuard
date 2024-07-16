4779f6fe41a7217e19b223b7a6f8f7fabaa69c24
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-07-16 18:34:56 +0100

    - Changes related to UI

--- desktop_app/ui/windows/student_register_info/student_register_info_ui.py
+++ desktop_app/ui/windows/student_register_info/student_register_info_ui.py
@@ -15,10 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDateEdit, QFormLayout, QHBoxLayout,
-    QLabel, QLineEdit, QMainWindow, QPushButton,
-    QSizePolicy, QSpacerItem, QStatusBar, QVBoxLayout,
-    QWidget)
+from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QComboBox, QDateEdit,
+    QFormLayout, QHBoxLayout, QLabel, QLineEdit,
+    QMainWindow, QPushButton, QSizePolicy, QSpacerItem,
+    QSpinBox, QStatusBar, QVBoxLayout, QWidget)
 
 class Ui_StudentRegisterInfo(object):
     def setupUi(self, StudentRegisterInfo):
@@ -39,6 +39,8 @@ class Ui_StudentRegisterInfo(object):
 
         self.formLayout_3 = QFormLayout()
         self.formLayout_3.setObjectName(u"formLayout_3")
+        self.formLayout_3.setFieldGrowthPolicy(QFormLayout.FieldGrowthPolicy.ExpandingFieldsGrow)
+        self.formLayout_3.setRowWrapPolicy(QFormLayout.RowWrapPolicy.DontWrapRows)
         self.formLayout_3.setLabelAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignVCenter)
         self.label_2 = QLabel(self.centralwidget)
         self.label_2.setObjectName(u"label_2")
@@ -57,6 +59,7 @@ class Ui_StudentRegisterInfo(object):
 
         self.birthday_input = QDateEdit(self.centralwidget)
         self.birthday_input.setObjectName(u"birthday_input")
+        self.birthday_input.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
 
         self.formLayout_3.setWidget(1, QFormLayout.FieldRole, self.birthday_input)
 
@@ -108,6 +111,11 @@ class Ui_StudentRegisterInfo(object):
 
         self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_7)
 
+        self.domain_input = QLineEdit(self.centralwidget)
+        self.domain_input.setObjectName(u"domain_input")
+
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.domain_input)
+
         self.label_8 = QLabel(self.centralwidget)
         self.label_8.setObjectName(u"label_8")
 
@@ -128,11 +136,6 @@ class Ui_StudentRegisterInfo(object):
 
         self.formLayout.setWidget(4, QFormLayout.FieldRole, self.departement_input)
 
-        self.domain_input = QLineEdit(self.centralwidget)
-        self.domain_input.setObjectName(u"domain_input")
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.domain_input)
-
 
         self.horizontalLayout_2.addLayout(self.formLayout)
 
@@ -142,52 +145,76 @@ class Ui_StudentRegisterInfo(object):
         self.label_10 = QLabel(self.centralwidget)
         self.label_10.setObjectName(u"label_10")
 
-        self.formLayout_2.setWidget(0, QFormLayout.LabelRole, self.label_10)
-
-        self.degree_input = QLineEdit(self.centralwidget)
-        self.degree_input.setObjectName(u"degree_input")
-
-        self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.degree_input)
+        self.formLayout_2.setWidget(1, QFormLayout.LabelRole, self.label_10)
 
         self.label_11 = QLabel(self.centralwidget)
         self.label_11.setObjectName(u"label_11")
 
-        self.formLayout_2.setWidget(1, QFormLayout.LabelRole, self.label_11)
-
-        self.preparation_year_input = QLineEdit(self.centralwidget)
-        self.preparation_year_input.setObjectName(u"preparation_year_input")
-
-        self.formLayout_2.setWidget(1, QFormLayout.FieldRole, self.preparation_year_input)
+        self.formLayout_2.setWidget(3, QFormLayout.LabelRole, self.label_11)
 
         self.label_12 = QLabel(self.centralwidget)
         self.label_12.setObjectName(u"label_12")
 
-        self.formLayout_2.setWidget(2, QFormLayout.LabelRole, self.label_12)
-
-        self.registration_year = QLineEdit(self.centralwidget)
-        self.registration_year.setObjectName(u"registration_year")
-
-        self.formLayout_2.setWidget(2, QFormLayout.FieldRole, self.registration_year)
+        self.formLayout_2.setWidget(4, QFormLayout.LabelRole, self.label_12)
 
         self.label_13 = QLabel(self.centralwidget)
         self.label_13.setObjectName(u"label_13")
 
-        self.formLayout_2.setWidget(3, QFormLayout.LabelRole, self.label_13)
+        self.formLayout_2.setWidget(5, QFormLayout.LabelRole, self.label_13)
 
-        self.delebration_year_input = QLineEdit(self.centralwidget)
-        self.delebration_year_input.setObjectName(u"delebration_year_input")
+        self.registration_year_spinbox = QSpinBox(self.centralwidget)
+        self.registration_year_spinbox.setObjectName(u"registration_year_spinbox")
+        self.registration_year_spinbox.setWrapping(False)
+        self.registration_year_spinbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.registration_year_spinbox.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.registration_year_spinbox.setAccelerated(True)
+        self.registration_year_spinbox.setProperty("showGroupSeparator", False)
+        self.registration_year_spinbox.setMinimum(1600)
+        self.registration_year_spinbox.setMaximum(2500)
+        self.registration_year_spinbox.setStepType(QAbstractSpinBox.StepType.DefaultStepType)
+        self.registration_year_spinbox.setValue(2000)
 
-        self.formLayout_2.setWidget(3, QFormLayout.FieldRole, self.delebration_year_input)
+        self.formLayout_2.setWidget(4, QFormLayout.FieldRole, self.registration_year_spinbox)
+
+        self.preparation_year_spinbox = QSpinBox(self.centralwidget)
+        self.preparation_year_spinbox.setObjectName(u"preparation_year_spinbox")
+        self.preparation_year_spinbox.setFrame(True)
+        self.preparation_year_spinbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.preparation_year_spinbox.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.preparation_year_spinbox.setMinimum(1600)
+        self.preparation_year_spinbox.setMaximum(2500)
+        self.preparation_year_spinbox.setValue(2000)
+
+        self.formLayout_2.setWidget(3, QFormLayout.FieldRole, self.preparation_year_spinbox)
+
+        self.delibration_year_spinbox = QSpinBox(self.centralwidget)
+        self.delibration_year_spinbox.setObjectName(u"delibration_year_spinbox")
+        self.delibration_year_spinbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.delibration_year_spinbox.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.delibration_year_spinbox.setMinimum(1600)
+        self.delibration_year_spinbox.setMaximum(2500)
+        self.delibration_year_spinbox.setValue(2000)
+
+        self.formLayout_2.setWidget(5, QFormLayout.FieldRole, self.delibration_year_spinbox)
+
+        self.degree_combobox = QComboBox(self.centralwidget)
+        self.degree_combobox.addItem("")
+        self.degree_combobox.addItem("")
+        self.degree_combobox.addItem("")
+        self.degree_combobox.setObjectName(u"degree_combobox")
+        self.degree_combobox.setEditable(False)
+
+        self.formLayout_2.setWidget(1, QFormLayout.FieldRole, self.degree_combobox)
 
         self.label_14 = QLabel(self.centralwidget)
         self.label_14.setObjectName(u"label_14")
 
-        self.formLayout_2.setWidget(4, QFormLayout.LabelRole, self.label_14)
+        self.formLayout_2.setWidget(0, QFormLayout.LabelRole, self.label_14)
 
         self.note_input = QLineEdit(self.centralwidget)
         self.note_input.setObjectName(u"note_input")
 
-        self.formLayout_2.setWidget(4, QFormLayout.FieldRole, self.note_input)
+        self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.note_input)
 
 
         self.horizontalLayout_2.addLayout(self.formLayout_2)
@@ -241,8 +268,9 @@ class Ui_StudentRegisterInfo(object):
         StudentRegisterInfo.setWindowTitle(QCoreApplication.translate("StudentRegisterInfo", u"MainWindow", None))
         self.student_picture.setText(QCoreApplication.translate("StudentRegisterInfo", u"Profile", None))
         self.label_2.setText(QCoreApplication.translate("StudentRegisterInfo", u"Name", None))
-        self.name_input.setText(QCoreApplication.translate("StudentRegisterInfo", u"TextLabel", None))
+        self.name_input.setText("")
         self.label_3.setText(QCoreApplication.translate("StudentRegisterInfo", u"Birthday", None))
+        self.birthday_input.setDisplayFormat(QCoreApplication.translate("StudentRegisterInfo", u"dd/MM/yyyy", None))
         self.label_4.setText(QCoreApplication.translate("StudentRegisterInfo", u"Place of birth", None))
         self.birth_place_input.setText("")
         self.label_6.setText(QCoreApplication.translate("StudentRegisterInfo", u"University", None))
@@ -250,19 +278,19 @@ class Ui_StudentRegisterInfo(object):
         self.label_5.setText(QCoreApplication.translate("StudentRegisterInfo", u"Speciality", None))
         self.speciality_input.setText("")
         self.label_7.setText(QCoreApplication.translate("StudentRegisterInfo", u"Domain", None))
+        self.domain_input.setText("")
         self.label_8.setText(QCoreApplication.translate("StudentRegisterInfo", u"Sector", None))
         self.sector_input.setText("")
         self.label_9.setText(QCoreApplication.translate("StudentRegisterInfo", u"Department", None))
         self.departement_input.setText("")
-        self.domain_input.setText("")
         self.label_10.setText(QCoreApplication.translate("StudentRegisterInfo", u"Degree", None))
-        self.degree_input.setText("")
         self.label_11.setText(QCoreApplication.translate("StudentRegisterInfo", u"Year Prepare", None))
-        self.preparation_year_input.setText("")
         self.label_12.setText(QCoreApplication.translate("StudentRegisterInfo", u"Year Registration", None))
-        self.registration_year.setText("")
         self.label_13.setText(QCoreApplication.translate("StudentRegisterInfo", u"Date Deliberation", None))
-        self.delebration_year_input.setText("")
+        self.degree_combobox.setItemText(0, QCoreApplication.translate("StudentRegisterInfo", u"Select Degree", None))
+        self.degree_combobox.setItemText(1, QCoreApplication.translate("StudentRegisterInfo", u"Bachelor", None))
+        self.degree_combobox.setItemText(2, QCoreApplication.translate("StudentRegisterInfo", u"Master", None))
+
         self.label_14.setText(QCoreApplication.translate("StudentRegisterInfo", u"Note", None))
         self.note_input.setText("")
         self.select_picture_button.setText(QCoreApplication.translate("StudentRegisterInfo", u"Select Image", None))
