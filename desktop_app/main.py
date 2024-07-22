a4c2e31909693fd9178b857d38aa5ed903ffd8fd
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-07-22 19:26:01 +0100

    - Register manualy 20 student

--- desktop_app/main.py
+++ desktop_app/main.py
@@ -9,20 +9,20 @@ class Main(Login, QMainWindow):
 		self.username_input.setText('admin')
 		self.password_input.setText('admin')
 		self.login()
-		self.student_manager_window.show_regiter_student_window()
-		
-		# self.student_manager_window.register_students_window.name_input.setText('geregr')
-		# self.student_manager_window.register_students_window.birth_place_input.setText('geregr')
-		# self.student_manager_window.register_students_window.university_input.setText('geregr') 
-		# self.student_manager_window.register_students_window.speciality_input.setText('geregr')
-		# self.student_manager_window.register_students_window.domain_input.setText('geregr')
-		# self.student_manager_window.register_students_window.sector_input.setText('geregr') 
-		# self.student_manager_window.register_students_window.departement_input.setText('geregr') 
-		# self.student_manager_window.register_students_window.degree_combobox.setCurrentIndex(1), 
-		# self.student_manager_window.register_students_window.note_input.setText('geregr') 
-		# self.student_manager_window.register_students_window.picture_path = "desktop_app/tests/test_images/alice.jpg"
-		
-		# self.student_manager_window.register_students_window.register_student()
+		# for student_id in range(1, 20):
+		# 	self.student_manager_window.show_regiter_student_window()
+		# 	self.student_manager_window.register_students_window.name_input.setText(f"student_{str(student_id)}")
+		# 	self.student_manager_window.register_students_window.birth_place_input.setText('geregr')
+		# 	self.student_manager_window.register_students_window.university_input.setText('geregr') 
+		# 	self.student_manager_window.register_students_window.speciality_input.setText('geregr')
+		# 	self.student_manager_window.register_students_window.domain_input.setText('geregr')
+		# 	self.student_manager_window.register_students_window.sector_input.setText('geregr') 
+		# 	self.student_manager_window.register_students_window.departement_input.setText('geregr') 
+		# 	self.student_manager_window.register_students_window.degree_combobox.setCurrentIndex(1), 
+		# 	self.student_manager_window.register_students_window.note_input.setText('geregr') 
+		# 	self.student_manager_window.register_students_window.picture_path = "desktop_app/tests/test_images/alice.jpg"
+			
+		# 	self.student_manager_window.register_students_window.register_student()
 		
 
 if __name__ == '__main__':
