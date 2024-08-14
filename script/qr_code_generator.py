c9ace6198611ba6208cf991fd7531a36e196a1c0
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-08-14 21:20:12 +0100

    - Change encrypted QR Code generation scripte - Remove key.bin and result.jpg file

--- script/qr_code_generator.py
+++ script/qr_code_generator.py
@@ -1,36 +1,56 @@
 import os
 import qrcode
-from base64 import b64encode, b64decode
+from base64 import b64encode
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
+from Crypto.Util.Padding import pad
 from Crypto.Random import get_random_bytes
-import json
 
-data = b"this is a highly secret message please don't tell anyone save it with you life got it ?"
-directory = os.path.join(os.getcwd(), 'DesktopAPP')
+directory = os.path.join(os.getcwd(), 'script')
 
-# Check if the key is loaded if not then create it 
-try:
-    with open(os.path.join(directory, 'key.bin'), 'rb') as file:
-        key = file.read()
-except FileNotFoundError:
-    key = get_random_bytes(16)
-    with open(os.path.join(directory, 'key.bin'), 'wb') as file:
-        file.write(key)
+def get_data_to_code():
+    global directory
+    data = input('Please Enter the Data you want to encrypt : ')
+    data = data.encode("utf-8")
+    # Check if there is a key file and if not create one
+    key_path = os.path.join(directory, 'key.bin')
+    if os.path.exists(key_path):
+        response = input(f"A key file already exists at {key_path}. Do you want to create a new key? (y/N): ")
+        if response.lower() == 'y':
+            key = get_random_bytes(16)
+            with open(key_path, 'wb') as file:
+                file.write(key)
+        else:
+            with open(key_path, 'rb') as file:
+                key = file.read()
+    else:
+        key = get_random_bytes(16)
+        with open(key_path, 'wb') as file:
+            file.write(key)
+    return data, key
 
-cipher = AES.new(key, AES.MODE_CBC)
-encrypted_data = b64encode(cipher.iv + cipher.encrypt(pad(data, AES.block_size)))
+def encrypt_text(data, key):
+    cipher = AES.new(key, AES.MODE_CBC)
+    encrypted_data = b64encode(cipher.iv + cipher.encrypt(pad(data, AES.block_size)))
+    return encrypted_data
 
-print(f'Data: {data}\n')
-print(f'Key is : {key} in Bytes\n')
-print(f'Key length is : {len(key)} in Bytes\n')
-print(f'Key is : {b64encode(key)} in Base64\n')
-print(f'Key length is : {len(b64encode(key))} in Base64\n')
+def generate_qr(encrypted_data, key):
+    global directory
+    # Generate encrypted QR Code & save it in result.jpg
+    img = qrcode.make(encrypted_data+b64encode(key))
+    img.save(os.path.join(directory, 'result.jpg'))
 
-print(f'the encrypted Data :{encrypted_data +b64encode(key)}')
+if __name__ == '__main__':
+    data, key = get_data_to_code()
+    encrypted_data = encrypt_text(data, key)
+    generate_qr(encrypted_data, key)
 
-# Generate QR Code using the encrypted data and saving it
-img = qrcode.make(encrypted_data+b64encode(key))
-img.save(os.path.join(directory, 'result.jpg'))
+    # Simple print statement to show the encrypted data
+    print(f'Data: {data}\n')
+    print(f'Key is : {key} in Bytes\n')
+    print(f'Key length is : {len(key)} in Bytes\n')
+    print(f'Key is : {b64encode(key)} in Base64\n')
+    print(f'Key length is : {len(b64encode(key))} in Base64\n')
+    print(f'the encrypted Data :{encrypted_data +b64encode(key)}')
 
-print("Script Complete")
+    # Print statement to show that the script is complete
+    print("Script Complete")
