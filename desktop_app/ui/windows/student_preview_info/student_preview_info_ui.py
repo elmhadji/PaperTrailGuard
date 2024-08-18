fbaf4ced0ae15d1dd43769fe080bc916b3f7f8e9
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-08-18 18:40:08 +0100

    - Rename object to proper names - Change all input widget to read only -  Change some input from Edit text to proper input

--- desktop_app/ui/windows/student_preview_info/student_preview_info_ui.py
+++ desktop_app/ui/windows/student_preview_info/student_preview_info_ui.py
@@ -15,9 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDateEdit, QFormLayout, QHBoxLayout,
-    QLabel, QLineEdit, QMainWindow, QSizePolicy,
-    QStatusBar, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QAbstractSpinBox, QApplication, QDateEdit, QFormLayout,
+    QHBoxLayout, QLabel, QLineEdit, QMainWindow,
+    QSizePolicy, QSpinBox, QStatusBar, QVBoxLayout,
+    QWidget)
 
 class Ui_StudentPreviewInfo(object):
     def setupUi(self, StudentPreviewInfo):
@@ -30,11 +31,11 @@ class Ui_StudentPreviewInfo(object):
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.student_picture = QLabel(self.centralwidget)
+        self.student_picture.setObjectName(u"student_picture")
+        self.student_picture.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        self.verticalLayout.addWidget(self.label)
+        self.verticalLayout.addWidget(self.student_picture)
 
         self.formLayout_3 = QFormLayout()
         self.formLayout_3.setObjectName(u"formLayout_3")
@@ -44,30 +45,34 @@ class Ui_StudentPreviewInfo(object):
 
         self.formLayout_3.setWidget(0, QFormLayout.LabelRole, self.label_2)
 
-        self.lineEdit = QLineEdit(self.centralwidget)
-        self.lineEdit.setObjectName(u"lineEdit")
+        self.name_input = QLineEdit(self.centralwidget)
+        self.name_input.setObjectName(u"name_input")
+        self.name_input.setReadOnly(True)
 
-        self.formLayout_3.setWidget(0, QFormLayout.FieldRole, self.lineEdit)
+        self.formLayout_3.setWidget(0, QFormLayout.FieldRole, self.name_input)
 
         self.label_3 = QLabel(self.centralwidget)
         self.label_3.setObjectName(u"label_3")
 
         self.formLayout_3.setWidget(1, QFormLayout.LabelRole, self.label_3)
 
-        self.dateEdit = QDateEdit(self.centralwidget)
-        self.dateEdit.setObjectName(u"dateEdit")
+        self.birthday_input = QDateEdit(self.centralwidget)
+        self.birthday_input.setObjectName(u"birthday_input")
+        self.birthday_input.setReadOnly(True)
+        self.birthday_input.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
 
-        self.formLayout_3.setWidget(1, QFormLayout.FieldRole, self.dateEdit)
+        self.formLayout_3.setWidget(1, QFormLayout.FieldRole, self.birthday_input)
 
         self.label_4 = QLabel(self.centralwidget)
         self.label_4.setObjectName(u"label_4")
 
         self.formLayout_3.setWidget(2, QFormLayout.LabelRole, self.label_4)
 
-        self.lineEdit_2 = QLineEdit(self.centralwidget)
-        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.birth_place_input = QLineEdit(self.centralwidget)
+        self.birth_place_input.setObjectName(u"birth_place_input")
+        self.birth_place_input.setReadOnly(True)
 
-        self.formLayout_3.setWidget(2, QFormLayout.FieldRole, self.lineEdit_2)
+        self.formLayout_3.setWidget(2, QFormLayout.FieldRole, self.birth_place_input)
 
 
         self.verticalLayout.addLayout(self.formLayout_3)
@@ -83,20 +88,22 @@ class Ui_StudentPreviewInfo(object):
 
         self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label_6)
 
-        self.lineEdit_4 = QLineEdit(self.centralwidget)
-        self.lineEdit_4.setObjectName(u"lineEdit_4")
+        self.university_input = QLineEdit(self.centralwidget)
+        self.university_input.setObjectName(u"university_input")
+        self.university_input.setReadOnly(True)
 
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.lineEdit_4)
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.university_input)
 
         self.label_5 = QLabel(self.centralwidget)
         self.label_5.setObjectName(u"label_5")
 
         self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_5)
 
-        self.lineEdit_3 = QLineEdit(self.centralwidget)
-        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.speciality_input = QLineEdit(self.centralwidget)
+        self.speciality_input.setObjectName(u"speciality_input")
+        self.speciality_input.setReadOnly(True)
 
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.lineEdit_3)
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.speciality_input)
 
         self.label_7 = QLabel(self.centralwidget)
         self.label_7.setObjectName(u"label_7")
@@ -108,25 +115,28 @@ class Ui_StudentPreviewInfo(object):
 
         self.formLayout.setWidget(3, QFormLayout.LabelRole, self.label_8)
 
-        self.lineEdit_6 = QLineEdit(self.centralwidget)
-        self.lineEdit_6.setObjectName(u"lineEdit_6")
+        self.sector_input = QLineEdit(self.centralwidget)
+        self.sector_input.setObjectName(u"sector_input")
+        self.sector_input.setReadOnly(True)
 
-        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.lineEdit_6)
+        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.sector_input)
 
         self.label_9 = QLabel(self.centralwidget)
         self.label_9.setObjectName(u"label_9")
 
         self.formLayout.setWidget(4, QFormLayout.LabelRole, self.label_9)
 
-        self.lineEdit_7 = QLineEdit(self.centralwidget)
-        self.lineEdit_7.setObjectName(u"lineEdit_7")
+        self.departement_input = QLineEdit(self.centralwidget)
+        self.departement_input.setObjectName(u"departement_input")
+        self.departement_input.setReadOnly(True)
 
-        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.lineEdit_7)
+        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.departement_input)
 
-        self.lineEdit_5 = QLineEdit(self.centralwidget)
-        self.lineEdit_5.setObjectName(u"lineEdit_5")
+        self.domain_input = QLineEdit(self.centralwidget)
+        self.domain_input.setObjectName(u"domain_input")
+        self.domain_input.setReadOnly(True)
 
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.lineEdit_5)
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.domain_input)
 
 
         self.horizontalLayout.addLayout(self.formLayout)
@@ -139,50 +149,73 @@ class Ui_StudentPreviewInfo(object):
 
         self.formLayout_2.setWidget(0, QFormLayout.LabelRole, self.label_10)
 
-        self.lineEdit_8 = QLineEdit(self.centralwidget)
-        self.lineEdit_8.setObjectName(u"lineEdit_8")
-
-        self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.lineEdit_8)
-
         self.label_11 = QLabel(self.centralwidget)
         self.label_11.setObjectName(u"label_11")
 
         self.formLayout_2.setWidget(1, QFormLayout.LabelRole, self.label_11)
-
-        self.lineEdit_9 = QLineEdit(self.centralwidget)
-        self.lineEdit_9.setObjectName(u"lineEdit_9")
-
-        self.formLayout_2.setWidget(1, QFormLayout.FieldRole, self.lineEdit_9)
 
         self.label_12 = QLabel(self.centralwidget)
         self.label_12.setObjectName(u"label_12")
 
         self.formLayout_2.setWidget(2, QFormLayout.LabelRole, self.label_12)
 
-        self.lineEdit_10 = QLineEdit(self.centralwidget)
-        self.lineEdit_10.setObjectName(u"lineEdit_10")
-
-        self.formLayout_2.setWidget(2, QFormLayout.FieldRole, self.lineEdit_10)
-
         self.label_13 = QLabel(self.centralwidget)
         self.label_13.setObjectName(u"label_13")
 
         self.formLayout_2.setWidget(3, QFormLayout.LabelRole, self.label_13)
-
-        self.lineEdit_11 = QLineEdit(self.centralwidget)
-        self.lineEdit_11.setObjectName(u"lineEdit_11")
-
-        self.formLayout_2.setWidget(3, QFormLayout.FieldRole, self.lineEdit_11)
 
         self.label_14 = QLabel(self.centralwidget)
         self.label_14.setObjectName(u"label_14")
 
         self.formLayout_2.setWidget(4, QFormLayout.LabelRole, self.label_14)
 
-        self.lineEdit_12 = QLineEdit(self.centralwidget)
-        self.lineEdit_12.setObjectName(u"lineEdit_12")
+        self.note_input = QLineEdit(self.centralwidget)
+        self.note_input.setObjectName(u"note_input")
+        self.note_input.setReadOnly(True)
 
-        self.formLayout_2.setWidget(4, QFormLayout.FieldRole, self.lineEdit_12)
+        self.formLayout_2.setWidget(4, QFormLayout.FieldRole, self.note_input)
+
+        self.preparation_year_spinbox = QSpinBox(self.centralwidget)
+        self.preparation_year_spinbox.setObjectName(u"preparation_year_spinbox")
+        self.preparation_year_spinbox.setWrapping(False)
+        self.preparation_year_spinbox.setFrame(True)
+        self.preparation_year_spinbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.preparation_year_spinbox.setReadOnly(True)
+        self.preparation_year_spinbox.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.preparation_year_spinbox.setProperty("showGroupSeparator", False)
+        self.preparation_year_spinbox.setMinimum(1600)
+        self.preparation_year_spinbox.setMaximum(2500)
+        self.preparation_year_spinbox.setValue(2000)
+
+        self.formLayout_2.setWidget(1, QFormLayout.FieldRole, self.preparation_year_spinbox)
+
+        self.registration_year_spinbox = QSpinBox(self.centralwidget)
+        self.registration_year_spinbox.setObjectName(u"registration_year_spinbox")
+        self.registration_year_spinbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.registration_year_spinbox.setReadOnly(True)
+        self.registration_year_spinbox.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.registration_year_spinbox.setMinimum(1600)
+        self.registration_year_spinbox.setMaximum(2500)
+        self.registration_year_spinbox.setValue(2000)
+
+        self.formLayout_2.setWidget(2, QFormLayout.FieldRole, self.registration_year_spinbox)
+
+        self.delibration_year_spinbox = QSpinBox(self.centralwidget)
+        self.delibration_year_spinbox.setObjectName(u"delibration_year_spinbox")
+        self.delibration_year_spinbox.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.delibration_year_spinbox.setReadOnly(True)
+        self.delibration_year_spinbox.setButtonSymbols(QAbstractSpinBox.ButtonSymbols.NoButtons)
+        self.delibration_year_spinbox.setMinimum(1600)
+        self.delibration_year_spinbox.setMaximum(2500)
+        self.delibration_year_spinbox.setValue(2000)
+
+        self.formLayout_2.setWidget(3, QFormLayout.FieldRole, self.delibration_year_spinbox)
+
+        self.degree = QLineEdit(self.centralwidget)
+        self.degree.setObjectName(u"degree")
+        self.degree.setReadOnly(True)
+
+        self.formLayout_2.setWidget(0, QFormLayout.FieldRole, self.degree)
 
 
         self.horizontalLayout.addLayout(self.formLayout_2)
@@ -198,32 +231,29 @@ class Ui_StudentPreviewInfo(object):
     # setupUi
 
     def retranslateUi(self, StudentPreviewInfo):
-        StudentPreviewInfo.setWindowTitle(QCoreApplication.translate("StudentPreviewInfo", u"MainWindow", None))
-        self.label.setText(QCoreApplication.translate("StudentPreviewInfo", u"Profile", None))
+        StudentPreviewInfo.setWindowTitle(QCoreApplication.translate("StudentPreviewInfo", u"Preview Student", None))
+        self.student_picture.setText(QCoreApplication.translate("StudentPreviewInfo", u"Profile", None))
         self.label_2.setText(QCoreApplication.translate("StudentPreviewInfo", u"Name", None))
-        self.lineEdit.setText(QCoreApplication.translate("StudentPreviewInfo", u"TextLabel", None))
+        self.name_input.setText("")
         self.label_3.setText(QCoreApplication.translate("StudentPreviewInfo", u"Birthday", None))
+        self.birthday_input.setDisplayFormat(QCoreApplication.translate("StudentPreviewInfo", u"dd_MM_yyyy", None))
         self.label_4.setText(QCoreApplication.translate("StudentPreviewInfo", u"Place of birth", None))
-        self.lineEdit_2.setText("")
+        self.birth_place_input.setText("")
         self.label_6.setText(QCoreApplication.translate("StudentPreviewInfo", u"University", None))
-        self.lineEdit_4.setText("")
+        self.university_input.setText("")
         self.label_5.setText(QCoreApplication.translate("StudentPreviewInfo", u"Speciality", None))
-        self.lineEdit_3.setText("")
+        self.speciality_input.setText("")
         self.label_7.setText(QCoreApplication.translate("StudentPreviewInfo", u"Domain", None))
         self.label_8.setText(QCoreApplication.translate("StudentPreviewInfo", u"Sector", None))
-        self.lineEdit_6.setText("")
+        self.sector_input.setText("")
         self.label_9.setText(QCoreApplication.translate("StudentPreviewInfo", u"Department", None))
-        self.lineEdit_7.setText("")
-        self.lineEdit_5.setText("")
+        self.departement_input.setText("")
+        self.domain_input.setText("")
         self.label_10.setText(QCoreApplication.translate("StudentPreviewInfo", u"Degree", None))
-        self.lineEdit_8.setText("")
         self.label_11.setText(QCoreApplication.translate("StudentPreviewInfo", u"Year Prepare", None))
-        self.lineEdit_9.setText("")
         self.label_12.setText(QCoreApplication.translate("StudentPreviewInfo", u"Year Registration", None))
-        self.lineEdit_10.setText("")
         self.label_13.setText(QCoreApplication.translate("StudentPreviewInfo", u"Date Deliberation", None))
-        self.lineEdit_11.setText("")
         self.label_14.setText(QCoreApplication.translate("StudentPreviewInfo", u"Note", None))
-        self.lineEdit_12.setText("")
+        self.note_input.setText("")
     # retranslateUi
 
