c1ac76fa95e7a265aeeb87ecf07712b3ce050bd1
ELMHADJI Mohamed Abdelhake <bahrib665@gmail.com>
2024-08-19 19:49:13 +0100

    - Implement student manager css

---
+++ desktop_app/ui/windows/student_manager/student_manager_rc.py
@@ -0,0 +1,238 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.7.1
+# WARNING! All changes made in this file will be lost!
+
+from PySide6 import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x02\x02\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 448 512\x22><!-\
+-!Font Awesome F\
+ree 6.6.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license/fr\
+ee Copyright 202\
+4 Fonticons, Inc\
+.--><path fill=\x22\
+#ffffff\x22 d=\x22M135\
+.2 17.7L128 32 3\
+2 32C14.3 32 0 4\
+6.3 0 64S14.3 96\
+ 32 96l384 0c17.\
+7 0 32-14.3 32-3\
+2s-14.3-32-32-32\
+l-96 0-7.2-14.3C\
+307.4 6.8 296.3 \
+0 284.2 0L163.8 \
+0c-12.1 0-23.2 6\
+.8-28.6 17.7zM41\
+6 128L32 128 53.\
+2 467c1.6 25.3 2\
+2.6 45 47.9 45l2\
+45.8 0c25.3 0 46\
+.3-19.7 47.9-45L\
+416 128z\x22/></svg\
+>\
+\x00\x00\x02n\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 640 512\x22><!-\
+-!Font Awesome F\
+ree 6.6.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license/fr\
+ee Copyright 202\
+4 Fonticons, Inc\
+.--><path fill=\x22\
+#ffffff\x22 d=\x22M96 \
+128a128 128 0 1 \
+1 256 0A128 128 \
+0 1 1 96 128zM0 \
+482.3C0 383.8 79\
+.8 304 178.3 304\
+l91.4 0C368.2 30\
+4 448 383.8 448 \
+482.3c0 16.4-13.\
+3 29.7-29.7 29.7\
+L29.7 512C13.3 5\
+12 0 498.7 0 482\
+.3zM504 312l0-64\
+-64 0c-13.3 0-24\
+-10.7-24-24s10.7\
+-24 24-24l64 0 0\
+-64c0-13.3 10.7-\
+24 24-24s24 10.7\
+ 24 24l0 64 64 0\
+c13.3 0 24 10.7 \
+24 24s-10.7 24-2\
+4 24l-64 0 0 64c\
+0 13.3-10.7 24-2\
+4 24s-24-10.7-24\
+-24z\x22/></svg>\
+\x00\x00\x01\xde\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 512 512\x22><!-\
+-!Font Awesome F\
+ree 6.6.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license/fr\
+ee Copyright 202\
+4 Fonticons, Inc\
+.--><path fill=\x22\
+#ffffff\x22 d=\x22M416\
+ 208c0 45.9-14.9\
+ 88.3-40 122.7L5\
+02.6 457.4c12.5 \
+12.5 12.5 32.8 0\
+ 45.3s-32.8 12.5\
+-45.3 0L330.7 37\
+6c-34.4 25.2-76.\
+8 40-122.7 40C93\
+.1 416 0 322.9 0\
+ 208S93.1 0 208 \
+0S416 93.1 416 2\
+08zM208 352a144 \
+144 0 1 0 0-288 \
+144 144 0 1 0 0 \
+288z\x22/></svg>\
+\x00\x00\x03\xa0\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 viewBox=\x22\
+0 0 512 512\x22><!-\
+-!Font Awesome F\
+ree 6.6.0 by @fo\
+ntawesome - http\
+s://fontawesome.\
+com License - ht\
+tps://fontawesom\
+e.com/license/fr\
+ee Copyright 202\
+4 Fonticons, Inc\
+.--><path fill=\x22\
+#ffffff\x22 d=\x22M142\
+.9 142.9c-17.5 1\
+7.5-30.1 38-37.8\
+ 59.8c-5.9 16.7-\
+24.2 25.4-40.8 1\
+9.5s-25.4-24.2-1\
+9.5-40.8C55.6 15\
+0.7 73.2 122 97.\
+6 97.6c87.2-87.2\
+ 228.3-87.5 315.\
+8-1L455 55c6.9-6\
+.9 17.2-8.9 26.2\
+-5.2s14.8 12.5 1\
+4.8 22.2l0 128c0\
+ 13.3-10.7 24-24\
+ 24l-8.4 0c0 0 0\
+ 0 0 0L344 224c-\
+9.7 0-18.5-5.8-2\
+2.2-14.8s-1.7-19\
+.3 5.2-26.2l41.1\
+-41.1c-62.6-61.5\
+-163.1-61.2-225.\
+3 1zM16 312c0-13\
+.3 10.7-24 24-24\
+l7.6 0 .7 0L168 \
+288c9.7 0 18.5 5\
+.8 22.2 14.8s1.7\
+ 19.3-5.2 26.2l-\
+41.1 41.1c62.6 6\
+1.5 163.1 61.2 2\
+25.3-1c17.5-17.5\
+ 30.1-38 37.8-59\
+.8c5.9-16.7 24.2\
+-25.4 40.8-19.5s\
+25.4 24.2 19.5 4\
+0.8c-10.8 30.6-2\
+8.4 59.3-52.9 83\
+.8c-87.2 87.2-22\
+8.3 87.5-315.8 1\
+L57 457c-6.9 6.9\
+-17.2 8.9-26.2 5\
+.2S16 449.7 16 4\
+40l0-119.6 0-.7 \
+0-7.6z\x22/></svg>\
+"
+
+qt_resource_name = b"\
+\x00\x05\
+\x00O\xa6S\
+\x00I\
+\x00c\x00o\x00n\x00s\
+\x00\x06\
+\x06\x8a\x9c\xb3\
+\x00a\
+\x00s\x00s\x00e\x00t\x00s\
+\x00\x05\
+\x00o\xa6S\
+\x00i\
+\x00c\x00o\x00n\x00s\
+\x00\x15\
+\x07)\xc4\xe7\
+\x00t\
+\x00r\x00a\x00s\x00h\x00-\x00s\x00o\x00l\x00i\x00d\x00-\x00w\x00h\x00i\x00t\x00e\
+\x00.\x00s\x00v\x00g\
+\x00\x13\
+\x07\x0a\x9c'\
+\x00u\
+\x00s\x00e\x00r\x00-\x00p\x00l\x00u\x00s\x00-\x00s\x00o\x00l\x00i\x00d\x00.\x00s\
+\x00v\x00g\
+\x00\x1a\
+\x0d\xbc\xb2'\
+\x00m\
+\x00a\x00g\x00n\x00i\x00f\x00y\x00i\x00n\x00g\x00-\x00g\x00l\x00a\x00s\x00s\x00-\
+\x00s\x00o\x00l\x00i\x00d\x00.\x00s\x00v\x00g\
+\x00\x10\
+\x03\xa1\x94\xa7\
+\x00r\
+\x00o\x00t\x00a\x00t\x00e\x00-\x00s\x00o\x00l\x00i\x00d\x00.\x00s\x00v\x00g\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x10\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x22\x00\x02\x00\x00\x00\x04\x00\x00\x00\x04\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\xc8\x00\x00\x00\x00\x00\x01\x00\x00\x06Z\
+\x00\x00\x01\x91k\xb7\xf8q\
+\x00\x00\x00b\x00\x00\x00\x00\x00\x01\x00\x00\x02\x06\
+\x00\x00\x01\x91k\xb71\x16\
+\x00\x00\x002\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x91k\xba\x13\xcc\
+\x00\x00\x00\x8e\x00\x00\x00\x00\x00\x01\x00\x00\x04x\
+\x00\x00\x01\x91k\xb6\xd3\xb2\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
--- desktop_app/ui/windows/student_manager/student_manager_ui.py
+++ desktop_app/ui/windows/student_manager/student_manager_ui.py
@@ -20,12 +20,40 @@ from PySide6.QtWidgets import (QAbstractItemView, QApplication, QCheckBox, QComb
     QLineEdit, QListView, QListWidget, QListWidgetItem,
     QMainWindow, QPushButton, QSizePolicy, QSpacerItem,
     QStatusBar, QWidget)
+from . import student_manager_rc
 
 class Ui_StudentManager(object):
     def setupUi(self, StudentManager):
         if not StudentManager.objectName():
             StudentManager.setObjectName(u"StudentManager")
         StudentManager.resize(767, 558)
+        StudentManager.setStyleSheet(u"#StudentManager {\n"
+"	font: 12pt \"Noto Sans\";\n"
+"}\n"
+"\n"
+"#search_button, #register_button, #delete_selected_button, #refresh_button, QLineEdit{\n"
+"	border: 1px solid  rgb(0, 94, 193);\n"
+"	border-radius: 10px;\n"
+"	padding: 10px;\n"
+"}\n"
+"#degree_combo_box {\n"
+"	padding: 5px;\n"
+"}\n"
+"\n"
+"#search_button, #register_button, #delete_selected_button, #refresh_button {\n"
+"	background-color: rgb(0, 94, 193);\n"
+"	color: rgb(255, 255, 255);\n"
+"}\n"
+"\n"
+"#search_button::hover, #register_button::hover, #refresh_button::hover {\n"
+"	background-color: #0066cc;\n"
+"}\n"
+"\n"
+"#delete_selected_button::hover {\n"
+"	background-color: red;\n"
+"	border: 1px solid  red;\n"
+"}\n"
+"")
         self.centralwidget = QWidget(StudentManager)
         self.centralwidget.setObjectName(u"centralwidget")
         self.gridLayout_2 = QGridLayout(self.centralwidget)
@@ -59,9 +87,14 @@ class Ui_StudentManager(object):
 
         self.search_button = QPushButton(self.centralwidget)
         self.search_button.setObjectName(u"search_button")
+        icon = QIcon()
+        icon.addFile(u":/Icons/assets/icons/magnifying-glass-solid.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.search_button.setIcon(icon)
 
         self.horizontalLayout.addWidget(self.search_button)
 
+        self.horizontalLayout.setStretch(1, 1)
+        self.horizontalLayout.setStretch(3, 2)
 
         self.horizontalLayout_2.addLayout(self.horizontalLayout)
 
@@ -71,9 +104,15 @@ class Ui_StudentManager(object):
 
         self.register_button = QPushButton(self.centralwidget)
         self.register_button.setObjectName(u"register_button")
+        icon1 = QIcon()
+        icon1.addFile(u":/Icons/assets/icons/user-plus-solid.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.register_button.setIcon(icon1)
 
         self.horizontalLayout_2.addWidget(self.register_button)
 
+        self.horizontalLayout_2.setStretch(0, 3)
+        self.horizontalLayout_2.setStretch(1, 1)
+        self.horizontalLayout_2.setStretch(2, 1)
 
         self.gridLayout_2.addLayout(self.horizontalLayout_2, 0, 0, 1, 1)
 
@@ -81,11 +120,15 @@ class Ui_StudentManager(object):
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.selected_all_checkBox = QCheckBox(self.centralwidget)
         self.selected_all_checkBox.setObjectName(u"selected_all_checkBox")
+        self.selected_all_checkBox.setTristate(False)
 
         self.horizontalLayout_3.addWidget(self.selected_all_checkBox)
 
         self.delete_selected_button = QPushButton(self.centralwidget)
         self.delete_selected_button.setObjectName(u"delete_selected_button")
+        icon2 = QIcon()
+        icon2.addFile(u":/Icons/assets/icons/trash-solid-white.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.delete_selected_button.setIcon(icon2)
 
         self.horizontalLayout_3.addWidget(self.delete_selected_button)
 
@@ -95,9 +138,15 @@ class Ui_StudentManager(object):
 
         self.refresh_button = QPushButton(self.centralwidget)
         self.refresh_button.setObjectName(u"refresh_button")
+        icon3 = QIcon()
+        icon3.addFile(u":/Icons/assets/icons/rotate-solid.svg", QSize(), QIcon.Normal, QIcon.Off)
+        self.refresh_button.setIcon(icon3)
 
         self.horizontalLayout_3.addWidget(self.refresh_button)
 
+        self.horizontalLayout_3.setStretch(1, 1)
+        self.horizontalLayout_3.setStretch(2, 3)
+        self.horizontalLayout_3.setStretch(3, 1)
 
         self.gridLayout_2.addLayout(self.horizontalLayout_3, 1, 0, 1, 1)
 
@@ -110,20 +159,27 @@ class Ui_StudentManager(object):
         self.student_card_info_list = QListWidget(self.frame)
         self.student_card_info_list.setObjectName(u"student_card_info_list")
         self.student_card_info_list.setAcceptDrops(False)
+        self.student_card_info_list.setFrameShape(QFrame.Shape.StyledPanel)
+        self.student_card_info_list.setFrameShadow(QFrame.Shadow.Sunken)
+        self.student_card_info_list.setEditTriggers(QAbstractItemView.EditTrigger.NoEditTriggers)
         self.student_card_info_list.setProperty("showDropIndicator", True)
         self.student_card_info_list.setDragEnabled(False)
         self.student_card_info_list.setDragDropOverwriteMode(False)
-        self.student_card_info_list.setDragDropMode(QAbstractItemView.DragDropMode.DragOnly)
+        self.student_card_info_list.setDragDropMode(QAbstractItemView.DragDropMode.NoDragDrop)
         self.student_card_info_list.setDefaultDropAction(Qt.DropAction.LinkAction)
+        self.student_card_info_list.setAlternatingRowColors(False)
         self.student_card_info_list.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
+        self.student_card_info_list.setTextElideMode(Qt.TextElideMode.ElideLeft)
+        self.student_card_info_list.setHorizontalScrollMode(QAbstractItemView.ScrollMode.ScrollPerItem)
         self.student_card_info_list.setMovement(QListView.Movement.Snap)
         self.student_card_info_list.setFlow(QListView.Flow.LeftToRight)
         self.student_card_info_list.setProperty("isWrapping", True)
         self.student_card_info_list.setResizeMode(QListView.ResizeMode.Adjust)
         self.student_card_info_list.setLayoutMode(QListView.LayoutMode.Batched)
         self.student_card_info_list.setViewMode(QListView.ViewMode.ListMode)
+        self.student_card_info_list.setWordWrap(True)
         self.student_card_info_list.setSelectionRectVisible(True)
-        self.student_card_info_list.setItemAlignment(Qt.AlignmentFlag.AlignHCenter)
+        self.student_card_info_list.setItemAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.gridLayout.addWidget(self.student_card_info_list, 0, 0, 1, 1)
 
@@ -148,7 +204,7 @@ class Ui_StudentManager(object):
         self.degree_combo_box.setItemText(2, QCoreApplication.translate("StudentManager", u"Master", None))
 
         self.label_2.setText(QCoreApplication.translate("StudentManager", u"Name", None))
-        self.search_button.setText(QCoreApplication.translate("StudentManager", u"search", None))
+        self.search_button.setText("")
         self.register_button.setText(QCoreApplication.translate("StudentManager", u"Register", None))
         self.selected_all_checkBox.setText("")
         self.delete_selected_button.setText(QCoreApplication.translate("StudentManager", u"Delete Selected", None))
